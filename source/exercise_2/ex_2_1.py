a1b69881b47d1f822e9b5f2b1db7ed07a124f1b1
Ana Ferro Cantanhede <anacantanhede@fake.com>
2021-12-02 12:27:55 +0100

    exercises day 2

---
+++ source/exercise_2/ex_2_1.py
@@ -0,0 +1,25 @@
+class SubmarinePosition:
+    def __init__(self):
+        self.horizontal_position = 0
+        self.depth = 0
+
+    def move(self, direction, value):
+        if direction == 'forward':
+            self.horizontal_position+= int(value)
+        elif direction == 'down':
+            self.depth += int(value)
+        elif direction == 'up':
+            self.depth -= int(value)
+        else:
+            print("INVALID DIRECTION: ", direction)
+        return self
+    
+    def end_position(self):
+        return self.horizontal_position * self.depth
+
+position = SubmarinePosition()
+with open("./input.txt") as file:
+    for line in file:
+        direction, value = line.split()
+        position.move(direction, int(value))
+    print(position.end_position())
---
+++ source/exercise_2/ex_2_2.py
@@ -0,0 +1,27 @@
+class SubmarinePosition:
+    def __init__(self):
+        self.horizontal_position = 0
+        self.depth = 0
+        self.aim = 0
+
+    def move(self, direction, value):
+        if direction == 'forward':
+            self.horizontal_position+= int(value)
+            self.depth += self.aim * int(value)
+        elif direction == 'down':
+            self.aim += int(value)
+        elif direction == 'up':
+            self.aim -= int(value)
+        else:
+            print("INVALID DIRECTION: ", direction)
+        return self
+    
+    def end_position(self):
+        return self.horizontal_position * self.depth
+
+position = SubmarinePosition()
+with open("./input.txt") as file:
+    for line in file:
+        direction, value = line.split()
+        position.move(direction, int(value))
+    print(position.end_position())
