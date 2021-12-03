a75025aaf3d0e51c1845550305d5522ff8250408
Ana Ferro Cantanhede <anacantanhede@fake.com>
2021-12-03 11:51:57 +0100

    updated improvements of ex 1 in ex 2

--- source/exercise_3/binary_diagnostic_2.py
+++ source/exercise_3/binary_diagnostic_2.py
@@ -17,25 +17,22 @@ class SubmarineCondition:
 
     def determine_gamma_rate(self, input):
         # most common bit of each position from all numbers
-        temp_gamma_rate = [0] * len(input[0])
+        temp_gamma_rate = 0
         for i in range(len(input[0])):
-            count_0 = [column[i] for column in input].count("0")
             count_1 = [column[i] for column in input].count("1")
-            if count_1 > count_0:
-                temp_gamma_rate[i] = 1
-        self.gamma_rate_binary = temp_gamma_rate
-        self.gamma_rate = self._binary_list_to_decimal(temp_gamma_rate)
+            if count_1 * 2 > len(input):
+                temp_gamma_rate += 1 * (2 ** (len(input[0]) - 1 - i))
+        self.gamma_rate = temp_gamma_rate
         return self.gamma_rate
 
     def determine_epsilon_rate(self, input):
         # least common bit of each position from all numbers
-        temp_epsilon_rate = [0] * len(input[0])
+        temp_epsilon_rate = 0
         for i in range(len(input[0])):
-            count_0 = [column[i] for column in input].count("0")
             count_1 = [column[i] for column in input].count("1")
-            if count_1 < count_0:
-                temp_epsilon_rate[i] = 1
-        self.epsilon_rate = self._binary_list_to_decimal(temp_epsilon_rate)
+            if count_1 * 2 < len(input):
+                temp_epsilon_rate += 1 * (2 ** (len(input[0]) - 1 - i))
+        self.epsilon_rate = temp_epsilon_rate
         return self.epsilon_rate
 
     def determine_power_consumption(self):
